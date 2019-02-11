4b61fc6c953eb23a8384f9653aec3dbcee41dd88
AAsriyan <aasriyan@protonmail.com>
2019-02-11 18:31:41 -0500

    Added font awesome in footer file and added icons in footer

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from './components/CardComponents/CardContainer'
+import CardContainer from './components/CardComponents/CardContainer';
+import Footer from './components/FooterComponents/Footer';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
 
 const App = () => {
   return (
     <div className='app'>
       <HeaderContainer />
       <div className="card-container">
-        <a href="https://reactjs.org" target="_blank"><CardContainer /></a>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer"><CardContainer /></a>
       </div>
+      <Footer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import './HeaderContainer';
+const moment = require('moment');
+const currentDate = moment().format('ll');
 
 function HeaderTitle() {
   return (
     <div>
-      <h5 className="title">Lambda School<span> @LambdaSchool · 11 Feb</span></h5>
+      <h5 className="title">Lambda School<span> @LambdaSchool · {currentDate}</span></h5>
     </div>
   );
 }
