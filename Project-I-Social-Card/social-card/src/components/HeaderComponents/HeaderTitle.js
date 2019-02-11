30994e12cb9e379e5e101dc615c865245c999baf
AAsriyan <aasriyan@protonmail.com>
2019-02-11 16:54:03 -0500

    Created the component for social-card

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+import './HeaderContainer';
+
+function HeaderTitle() {
+  return (
+  <h5>Lambda School</h5>,
+  <h6>@LambdaSchool . 11 Feb</h6>
+  );
+}
+
+export default HeaderTitle;
