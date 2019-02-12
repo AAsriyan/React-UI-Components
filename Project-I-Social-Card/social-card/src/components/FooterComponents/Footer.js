79a2fceb5378657879a4c94c2b7ae921128dfa07
AAsriyan <aasriyan@protonmail.com>
2019-02-12 14:01:33 -0500

    Finishing up project

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -3,9 +3,6 @@ import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faRetweet, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-
-<script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
-
 function Footer() {
   return (
     <div className="footer-elements">
